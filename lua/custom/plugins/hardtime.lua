c4fac37ee18cf361a3aa816f96b867575f1d7720
Jose Pedro Oliveira <jose.p.oliveira.oss@gmail.com>
2025-05-19 00:12:56 +0200

    Plugin: add custom plugin hardtime.nvim

---
+++ lua/custom/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+return {
+  --  https://github.com/m4xshen/hardtime.nvim
+  'm4xshen/hardtime.nvim',
+  enable = true,
+  lazy = false,
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  opts = {},
+}
